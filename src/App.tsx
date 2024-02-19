0207e406071663fc762e773bbd774e1672f69249
omid mohebbi <omidmohebbise@gmail.com>
2024-02-19 10:24:58 +0330

    initial the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default App;
